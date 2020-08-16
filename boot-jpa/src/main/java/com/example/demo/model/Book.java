acb17d978e469e2e0442fc51a5581082f625e922
nakamura999 <masahiro9ama9@gmail.com>
2020-08-16 11:13:33 +0900

    boot jpa finish

--- boot-jpa/src/main/java/com/example/demo/model/Book.java
+++ boot-jpa/src/main/java/com/example/demo/model/Book.java
@@ -4,6 +4,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-public class Book {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+// @Entity データの入れ物
+// @Id エンティティの主キーを設定
+// @GeneratedValue 主キーの値を自動採番します。@Idアノテーションと一緒に設定
+public class Book {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String title;
+	private String detail;
 }
