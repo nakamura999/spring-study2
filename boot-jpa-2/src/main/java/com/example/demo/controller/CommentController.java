b7a3d1d94ddcaae785834a37eed1144599ec5e99
nakamura999 <masahiro9ama9@gmail.com>
2020-08-17 17:33:02 +0900

    controller dataloader edit

--- boot-jpa-2/src/main/java/com/example/demo/controller/CommentController.java
+++ boot-jpa-2/src/main/java/com/example/demo/controller/CommentController.java
@@ -14,9 +14,28 @@ import com.example.demo.repository.CommentRepository;
 @Controller
 public class CommentController {
 	private final CommentRepository repository;
-	
+	// フィールドをfinalにできる final　変更できないようにする
+
 	public CommentController(CommentRepository repository) {
 		this.repository = repository;
 	}
+	
+	@GetMapping("/")
+	public String getAllComment(@ModelAttribute Comment comment, Model model) {
+		model.addAttribute("comments", repository.findAll());
+		return "list";
+	}
+	
+	@PostMapping("/add")
+	public String addComment(@Validated @ModelAttribute Comment comment, BindingResult result, Model model) {
+		model.addAttribute("comments", repository.findAll());
+		if (result.hasErrors()) {
+			return "list";
+		}
+		repository.save(comment);
+		return "redirect:/";
+	}
+	// repository.findAll() データを全件取得
+	// repository.save(object) データを保存
 
 }
